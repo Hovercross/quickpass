56810776552c5d0994bb989851100b60e9c372ce
Adam Peacock <adam@thepeacock.net>
2024-04-10 14:46:25 -0400

    Better rule based password generation

--- main.py
+++ main.py
@@ -2,25 +2,21 @@
 
 from dataclasses import dataclass
 from secrets import choice
+from typing import Iterable, Any
+from functools import cached_property
 
 import argparse
 import sys
-
-LETTERS_LC = 'abcdefghijklmnoprqstuvwxyz'
-LETTERS_UC = LETTERS_LC.upper()
-NUMBERS = '0123456789'
+import string
 
 
 def main():
     options = get_options()
-    chars = LETTERS_UC + LETTERS_LC + NUMBERS
+    password = options.generate()
 
-    password = ''.join(choice(chars) for _ in range(options.length))
-
+    sys.stdout.write(password)
     if options.newline:
-        print(password)
-    else:
-        sys.stdout.write(password)
+        sys.stdout.write("\n")
 
 
 @dataclass
@@ -29,30 +25,172 @@ class Options:
 
     length: int
     newline: bool
+    lowercase: bool
+    uppercase: bool
+    numbers: bool
+    symbols: bool
+
+    @cached_property
+    def all_character_sets(self) -> list[str]:
+        out: list[str] = []
+
+        if self.lowercase:
+            out.append(string.ascii_lowercase)
+
+        if self.uppercase:
+            out.append(string.ascii_uppercase)
+
+        if self.numbers:
+            out.append(string.digits)
+
+        if self.symbols:
+            out.append(string.punctuation)
+
+        return out
+
+    @cached_property
+    def all_characters(self) -> str:
+        return "".join(self.all_character_sets)
+
+    @cached_property
+    def start_end_characters(self) -> str:
+        """Get the characters that are applicable
+        for the start or end of the password"""
+
+        if self.lowercase and self.uppercase:
+            return string.ascii_letters
+
+        if self.lowercase:
+            return string.ascii_lowercase
+
+        if self.uppercase:
+            return string.ascii_uppercase
+
+        if self.numbers:
+            return string.digits
+
+        if self.symbols:
+            return string.punctuation
+
+        return ""
+
+    def _generate_unchecked(self) -> str:
+        """Return a password that might not have the appropriate character sets"""
+
+        working = [choice(self.all_characters) for _ in range(self.length)]
+
+        return "".join(working)
+
+    @property
+    def _should_check_passwords(self) -> bool:
+        # We are going to skip all checks if we are working with super short passwords
+        return self.length >= len(self.all_character_sets)
+
+    def _password_is_ok(self, candidate: str) -> bool:
+        if candidate[0] not in self.start_end_characters:
+            return False
+
+        if candidate[-1] not in self.start_end_characters:
+            return False
+
+        letters_used = set(candidate)
+
+        # Make sure we have some overlap with the password and each of the
+        # character sets
+        for character_set in self.all_character_sets:
+            character_set_characters = set(character_set)
+            if not character_set_characters & letters_used:
+                return False
+
+        return True
+
+    def generate(self) -> str:
+        while True:
+            candidate = self._generate_unchecked()
+            if not self._should_check_passwords:
+                return candidate
+
+            if self._password_is_ok(candidate):
+                return candidate
 
 
 def get_options(*args) -> Options:
     """Get the options for execution"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--length", type=int, default=63)
+    parser.add_argument(
+        "-n",
+        "--num-characters",
+        type=int,
+        default=63,
+        dest="length",
+        help="The length of the password to generate",
+    )
+
+    parser.add_argument(
+        "-nl",
+        "--no-lowercase",
+        action="store_const",
+        const=False,
+        dest="lowercase",
+        default=True,
+        help="Do not include lowercase letters",
+    )
+
+    parser.add_argument(
+        "-nu",
+        "--no-uppercase",
+        action="store_const",
+        const=False,
+        dest="uppercase",
+        default=True,
+        help="Do not include uppercase letters",
+    )
+
+    parser.add_argument(
+        "-nd",
+        "--no-digits",
+        action="store_const",
+        const=False,
+        dest="digits",
+        default=True,
+        help="Do not include digits",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--symbols",
+        action="store_const",
+        const=True,
+        dest="symbols",
+        default=False,
+        help="Include symbols in password",
+    )
 
     newline_group = parser.add_mutually_exclusive_group()
 
-    newline_group.add_argument("--newline",
-                               action="store_const",
-                               const=True,
-                               dest="newline")
+    newline_group.add_argument(
+        "--newline",
+        action="store_const",
+        const=True,
+        dest="newline",
+    )
 
-    newline_group.add_argument("--no-newline",
-                               action="store_const",
-                               const=False,
-                               dest="newline")
+    newline_group.add_argument(
+        "--no-newline",
+        action="store_const",
+        const=False,
+        dest="newline",
+    )
 
     args = parser.parse_args(*args)
 
-    length = args.length
-    newline = args.newline
+    length: int = args.length  # type: ignore
+    newline: bool | None = args.newline  # type: ignore
+    lowercase: bool = args.lowercase  # type: ignore
+    uppercase: bool = args.uppercase  # type: ignore
+    digits: bool = args.digits  # type: ignore
+    symbols: bool = args.symbols  # type: ignore
 
     if newline is None:
         if sys.stdout.isatty():
@@ -60,7 +198,14 @@ def get_options(*args) -> Options:
         else:
             newline = False
 
-    return Options(length=length, newline=newline)
+    return Options(
+        length=length,
+        newline=newline,
+        lowercase=lowercase,
+        uppercase=uppercase,
+        numbers=digits,
+        symbols=symbols,
+    )
 
 
 if __name__ == "__main__":
